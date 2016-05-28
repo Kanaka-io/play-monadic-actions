54570d80bc1cad3836c6840f44062b98f8d5bc92
Valentin Kasas <valentin.kasas@gmail.com>
2016-05-28 11:09:01 +0200

    Move things around to cleanup imports

--- cats/app/io/kanaka/monadic/dsl/compat/cats.scala
+++ cats/app/io/kanaka/monadic/dsl/compat/cats.scala
@@ -1,8 +1,23 @@
-package io.kanaka.play.compat
+/*
+ * Copyright 2014
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.kanaka.monadic.dsl.compat
 
 import _root_.cats.{Functor, Monad}
-import _root_.cats.data.{Xor, Validated}
-import io.kanaka.play.{Step, StepOps}
+import _root_.cats.data.{Validated, Xor}
+import io.kanaka.monadic.dsl.{Step, StepOps}
 import play.api.mvc.Result
 
 import scala.concurrent.{ExecutionContext, Future}
---
+++ core/app/io/kanaka/monadic/dsl/package.scala
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2014
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.kanaka.monadic
+
+import play.api.data.validation.ValidationError
+import play.api.libs.json.JsPath
+import play.api.mvc.Result
+import play.api.data.Form
+import play.api.libs.json.JsResult
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
+
+/**
+ * @author Valentin Kasas
+ */
+package object dsl {
+
+  type JsErrorContent = Seq[(JsPath, Seq[ValidationError])]
+
+  private [dsl] def fromFuture[A](onFailure: Throwable => Result)(future: Future[A])(implicit ec: ExecutionContext): Step[A] =
+    Step(
+      run = future.map(Right[Result, A](_)).recover{case t: Throwable => Left[Result, A](onFailure(t))},
+      executionContext = ec
+    )
+
+  private [dsl] def fromFOption[A](onNone: => Result)(fOption: Future[Option[A]])(implicit ec: ExecutionContext): Step[A] =
+    Step(
+      run = fOption.map{
+        case Some(a) => Right(a)
+        case None => Left(onNone)
+      }(ec),
+      executionContext = ec
+    )
+
+  private [dsl] def fromFEither[A,B](onLeft: B => Result)(fEither: Future[Either[B,A]])(implicit ec: ExecutionContext): Step[A] =
+    Step(
+      run = fEither.map(_.left.map(onLeft)),
+      executionContext = ec
+    )
+
+  private [dsl] def fromOption[A](onNone: => Result)(option: Option[A])(implicit ec: ExecutionContext): Step[A] =
+    Step(
+      run = Future.successful(option.fold[Either[Result,A]](Left(onNone))(Right(_))),
+      executionContext = ec
+    )
+
+  private [dsl] def fromEither[A,B](onLeft: B => Result)(either: Either[B,A])(implicit ec: ExecutionContext): Step[A] =
+    Step(
+      run = Future.successful(either.left.map(onLeft)),
+      executionContext = ec
+    )
+
+  private [dsl] def fromJsResult[A](onJsError: JsErrorContent => Result)(jsResult: JsResult[A])(implicit ec: ExecutionContext): Step[A] =
+    Step(
+      run = Future.successful(jsResult.fold(err => Left(onJsError(err)), Right(_))),
+      executionContext = ec
+    )
+
+  private [dsl] def fromForm[A](onError: Form[A] => Result)(form: Form[A])(implicit ec: ExecutionContext): Step[A] =
+    Step(
+      run = Future.successful(form.fold(err => Left(onError(err)), Right(_))),
+      executionContext = ec
+    )
+
+  private [dsl] def fromBoolean(onFalse: => Result)(boolean: Boolean)(implicit ec:ExecutionContext): Step[Unit] =
+    Step(
+      run = Future.successful(if(boolean) Right(()) else Left(onFalse)),
+      executionContext = ec
+    )
+
+  private [dsl] def fromTry[A](onFailure: Throwable => Result)(tryValue: Try[A])(implicit ec: ExecutionContext):Step[A] =
+    Step(
+      run = Future.successful(tryValue match {
+        case Failure(t) => Left(onFailure(t))
+        case Success(v) => Right(v)
+      }),
+      executionContext = ec
+    )
+}
